989c282a196d5ba2cf8336b2b8f3f8517a486d07
Guilherme Reis <guilhermereisdl@gmail.com>
2024-01-04 03:50:12 -0300

    First commit

---
+++ BankAccountCSharp/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using BankAccountCSharp;
+using Accounts;
+
+namespace BankAccountCSharp { 
+class Program
+{
+    static void Main()
+    {
+        
+
+        Console.WriteLine("\n\n--------------------------\nWelcome to the Hayes Bank!\n--------------------------\n");
+        Console.WriteLine("Please insert your name: \n");
+        string accountName = Console.ReadLine();
+
+        Account account = new Account(accountName, 0);
+        Console.WriteLine("\nWelcome " + account.Name + " you have successfully created your account.");
+
+        bool stop = false;
+
+        while(!stop)
+            {
+                Console.Clear();
+                Console.WriteLine("\n\nPlease press the corresponding letter of the operation you want: Deposit (D), Withdraw (W) or stop (E):\n");
+
+                char operation = Console.ReadKey().KeyChar;
+                operation = char.ToLower(operation);
+
+                switch (operation)
+                {
+                    case 'd':
+                        Console.WriteLine("\n\nHow much you want to deposit?\n");
+                        string depositAmountString = Console.ReadLine();
+
+                        if (decimal.TryParse(depositAmountString, out decimal depositAmount))
+                        {
+                            account.MakeDeposit(depositAmount);
+                            Console.WriteLine("\nNew balance: " + account.Balance.ToString("0.00"));
+                            Console.WriteLine("\nEnd operation? (y/n)\n");
+                            char confirmEnd = Console.ReadKey().KeyChar;
+                            char end = char.ToLower(confirmEnd);
+                            if (end == 'y')
+                            {
+                                Console.WriteLine("\nEnding...");
+                            } else if (end == 'n')
+                            {
+                                stop = false;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                                
+                        }
+
+                        Console.WriteLine("\nInvalid input. Please try again.");
+                        break;
+
+                    case 'w':
+                        Console.WriteLine("\n\nHow much you want to withdraw?\n");
+                        string withdrawAmountString = Console.ReadLine();
+
+                        if (decimal.TryParse(withdrawAmountString, out decimal withdrawAmount))
+                        {
+                            account.MakeWithdraw(withdrawAmount);
+                            Console.WriteLine("\nNew balance: " + account.Balance);
+                            Console.WriteLine("\nEnd operation? (y/n)\n");
+                            char confirmEnd = Console.ReadKey().KeyChar;
+                            char end = char.ToLower(confirmEnd);
+                            if (end == 'y')
+                            {
+                                Console.WriteLine("\nEnding...");
+                            }
+                            else if (end == 'n')
+                            {
+                                stop = false;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        break;
+                    case 'e':
+                        stop = true;
+                        Console.WriteLine("\nStoping...");
+                        break;
+                    default:
+                        stop = true;
+                        Console.WriteLine("\nInvalid input, please try again.");
+                        stop = false;
+                        break;
+                }
+            }
+       
+    }
+}
+
+}
---
+++ BankAccountCSharp/entities/Account.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace Accounts
+{
+class Account
+    {
+        public string? Name { get; set; }
+        public decimal Balance { get; set; }
+
+        public Account() { }
+
+        public Account(string name, decimal currentBalance)
+        {
+            Name = name;
+            Balance = currentBalance;
+        }
+
+        public void ChangeName(string newName) { Name = newName; }
+
+        public decimal MakeDeposit(decimal amount)
+        {
+             return Balance += amount;
+        }
+
+        public decimal MakeWithdraw(decimal amount)
+        {
+            if(Balance < amount) {
+                Console.WriteLine("Invalid amount, your account don't have $ " + Balance);
+                return Balance;
+            } else
+            {
+                return Balance -= amount;
+            }
+            
+        }
+    }
+
+}
